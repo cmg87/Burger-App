98d00347b52c65e8da35f696762f96a525f96f35
Chris <chrisgoerler@gmail.com>
2018-06-19 17:28:46 -0400

    initialize server and database

---
+++ config/orm.js
@@ -0,0 +1,10 @@
+let connection = require("../config/connection.js");
+
+let orm = {
+
+    selectAll: ,
+
+    insertOne: ,
+
+    updateOne: ,
+};
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) values ('Awesome Burger');
+INSERT INTO burgers (burger_name) values ('Tasty Burger');
+INSERT INTO burgers (burger_name) values ('Bacon Burger');
